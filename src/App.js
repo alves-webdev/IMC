03242d3ca389303ff1cec5ffc32b3b44389c483f
Gabriel <gabrielsoaresalves@gmail.com>
2022-02-01 13:05:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const[peso, setPeso] = useState('');
+  const[altura, setAltura] = useState('');
+
+  const [mensagem, setMensagem] = useState ('');
+
+  function calcularIMC(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt)
+    
+    if (imc < 18.6){
+      setMensagem(" Você está abaixo do peso ideal!, seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 18.6 && imc < 24.9){
+      setMensagem(" Você está com seu peso ideal, seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 24.9 && imc < 34.9){
+      setMensagem(" Você está levemente acima do peso, seu IMC: " + imc.toFixed(2))
+    }else if(imc > 34.9){
+      setMensagem(" Cuidado! você está na zona de obesidade!, seu IMC: " + imc.toFixed(2))
+    }
+
+  }
+
+  return(
+    <div className="app">
+      <h1> calculadora IMC </h1>
+      <span>vamos calcular seu IMC</span>
+
+      <div className="area-input">
+        <input 
+        type="text"
+        placeholder="Peso em (KG) ex: 90"
+        value={peso}
+        onChange={ (e) => setPeso(e.target.value)}
+        />
+         <input 
+        type="text"
+        placeholder="Altura em (CM) ex: 180"
+        value={altura}
+        onChange={ (e) => setAltura(e.target.value)}
+        />
+
+        <button onClick={calcularIMC}>
+          calcular
+        </button>
+      </div>
+
+      <h2>{mensagem}</h2>
     </div>
-  );
+  )
 }
-
-export default App;
